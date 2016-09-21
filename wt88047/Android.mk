1673807cb27447a031aa5b925926c7dfc6382dfe
ssawrav <s.sawrav@gmail.com>
2016-09-21 16:41:13 +0530

    Remove ims related blobs
    Conflicts:
    	wt88047/wt88047-vendor.mk

--- wt88047/Android.mk
+++ wt88047/Android.mk
@@ -163,15 +163,6 @@ LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := imscmlibrary
-LOCAL_MODULE_OWNER := wingtech
-LOCAL_SRC_FILES := proprietary/framework/imscmlibrary.jar
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := qcnvitems
 LOCAL_MODULE_OWNER := wingtech
 LOCAL_SRC_FILES := proprietary/framework/qcnvitems.jar
